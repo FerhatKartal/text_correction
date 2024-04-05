0b5d2db1cc40e37a25974df607ce86abe13698d8
Ferhat <101355331+FerhatKartal@users.noreply.github.com>
2024-04-05 12:52:12 +0300

    Update pos_tag.py

--- pos_tag.py
+++ pos_tag.py
@@ -1,10 +1,1 @@
-#cümlemizdeki özel isimleri, sıfatları vb. bulur
 
-import nltk
-from nltk.tokenize import word_tokenize, sent_tokenize
-
-
-def posTag(text):
-    words = word_tokenize(text)
-    tagged = nltk.pos_tag(words)
-    return tagged
